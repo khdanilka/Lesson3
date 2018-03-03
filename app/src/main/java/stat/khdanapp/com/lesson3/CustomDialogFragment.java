2a126a8219da27092e54971416d7d0105ad14353
khdanilka <daniil.khalupskiy@gmail.com>
2018-03-03 11:44:55 +0500

    Initial commit

---
+++ app/src/main/java/stat/khdanapp/com/lesson3/CustomDialogFragment.java
@@ -0,0 +1,23 @@
+package stat.khdanapp.com.lesson3;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+
+
+public class CustomDialogFragment extends DialogFragment {
+    @NonNull
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
+        return builder
+                .setTitle("Диалоговое окно")
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setMessage("Для закрытия окна нажмите ОК")
+                .setPositiveButton("OK", null)
+                .setNegativeButton("Отмена", null)
+                .create();
+    }
+}
---
+++ app/src/main/java/stat/khdanapp/com/lesson3/MainActivity.java
@@ -0,0 +1,23 @@
+package stat.khdanapp.com.lesson3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CustomDialogFragment dialog = new CustomDialogFragment();
+                dialog.show(getSupportFragmentManager(), "custom");
+            }
+        });
+    }
+}
